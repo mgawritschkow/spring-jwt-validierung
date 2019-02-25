df37f37f0849d33c36f3f73e64d8f03ee4566299
Michael Gawritschkow <mgawritschkov@inconso.de>
2019-02-25 16:35:22 +0100

    Start project

---
+++ src/main/java/spring/example/springjwtvalidierung/SpringJwtValidierungApplication.java
@@ -0,0 +1,13 @@
+package spring.example.springjwtvalidierung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtValidierungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtValidierungApplication.class, args);
+	}
+
+}
